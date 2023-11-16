30dd3423472a2c0263f17727ad92754c16e111bd
Jacob McKitrick <jakemckit@gmail.com>
2023-11-16 10:07:30 -0500

    fixed input inside iframe

--- input.js
+++ input.js
@@ -2,7 +2,15 @@
 let inputDirection = { x: 0, y: 0 }
 let lastInputDirection = { x: 0, y: 0 }
 
-
+document.addEventListener('keydown', function (e) {
+    // Check if the event occurred inside the iframe
+    if (window.self !== window.top) {
+        // If inside the iframe, prevent the default behavior of arrow keys
+        if (e.key === 'ArrowUp' || e.key === 'ArrowDown' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+            e.preventDefault();
+        }
+    }
+});
 window.addEventListener('keydown', e => {
     switch (e.key) {
         case 'ArrowUp':
